8432dc341734c5e112775c322f8c53fddab40c71
Hardik Sanghvi <hardik@torquehq.dev>
2022-07-18 16:51:33 +0530

    POS blockchain initial init

---
+++ Main.py
@@ -0,0 +1,21 @@
+from Transaction import Transaction
+from Wallet import Wallet
+
+if __name__ == '__main__':
+
+    sender = 'sender'
+    receiver = 'receiver'
+    amount = 1
+    type = 'TRANSFER'
+
+    transaction = Transaction(sender, receiver, amount, type)
+
+    wallet = Wallet()
+    signature = wallet.sign(transaction.toJson())
+
+    # transaction.sign(signature)
+
+    signatureValid = Wallet.signatureValid(
+        transaction.toJson(), signature, wallet.publicKeyString())
+
+    print(signatureValid)
